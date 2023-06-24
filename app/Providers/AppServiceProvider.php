8b3b1cc646ca2f7d76b19201408a3a878c373bee
Odame <kwesi.danquah@daaseng.com>
2023-06-24 14:18:05 +0100

    force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         Response::macro('success', function ($data) {
             return response()->json([
