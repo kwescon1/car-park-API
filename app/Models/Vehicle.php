d57d208d37a340de9a886a4866bea7bde5cf0c18
Odame <kwesi.danquah@daaseng.com>
2023-06-26 23:31:36 +0100

    register observer and scope

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\UserVehicleScope;
+use App\Observers\VehicleObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vehicle extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    protected static function booted()
+    {
+        self::observe(VehicleObserver::class);
+
+        static::addGlobalScope(new UserVehicleScope);
+
+    }
 }
