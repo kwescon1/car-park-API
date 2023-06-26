5a2c9588f08919274dff999ff83e0da44bb92bb0
Odame <kwesi.danquah@daaseng.com>
2023-06-26 13:35:46 +0100

    logout user functionality

---
+++ app/Http/Controllers/Api/v1/Auth/LogoutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->success(null, null, Response::HTTP_NO_CONTENT);
+    }
+}
