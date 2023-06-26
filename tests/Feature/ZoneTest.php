2b8891e8af8d4c6c5b2e5bf811d673a64c93b0dd
Odame <kwesi.danquah@daaseng.com>
2023-06-27 00:55:38 +0100

    zones api test

--- tests/Feature/ZoneTest.php
+++ tests/Feature/ZoneTest.php
@@ -15,7 +15,9 @@ class ZoneTest extends TestCase
 
         $response->assertStatus(200)
             ->assertJsonStructure(['data'])
-            ->assertJsonCount(3, 'data')
+            ->assertJsonCount(3, 'data')->assertJsonStructure(['data' => [
+                ['*' => 'id', 'name', 'price_per_hour'],
+            ]])
             ->assertJsonPath('data.0.name', 'Green Zone');
     }
 }
