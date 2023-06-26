3bed02d7d8604ff037e2446cb6e3ca6a0d2dcadb
Odame <kwesi.danquah@daaseng.com>
2023-06-26 02:33:34 +0100

    authentication test

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_register_user(): void
+    {
+
+        $data = ['email' => 'foo@bar.com', 'password' => 'password', 'password_confirmation' => 'password', 'name' => 'kwesi'];
+
+        $response = $this->postJson('api/v1/auth/register', $data);
+
+        $response->assertStatus(201);
+
+        $response->assertJsonStructure(['data']);
+    }
+
+    public function test_register_user_with_valition_error()
+    {
+        $user = User::factory()->make([]);
+
+        $response = $this->postJson('api/v1/auth/register', $user->toArray());
+
+        $response->assertStatus(422);
+
+        $user = User::factory()->create();
+
+        $response = $this->postJson('api/v1/auth/register', $user->toArray());
+
+        $response->assertStatus(422);
+    }
+
+    public function test_login_user_with_right_credentials()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson('/api/v1/auth/login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['data']);
+
+    }
+
+    public function test_login_user_with_wrong_credentials()
+    {
+        $response = $this->postJson('/api/v1/auth/login', [
+            'email' => 'nonexisting@user.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(422);
+
+    }
+}
