13a4394c17395cd7f7920798e46fc489bd5d803b
Odame <kwesi.danquah@daaseng.com>
2023-06-26 09:29:00 +0100

    update user profile

---
+++ app/Http/Controllers/Api/v1/Auth/ProfileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateProfileRequest;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    //
+    public function show(Request $request)
+    {
+        return response()->success($request->user()->only('name', 'email'));
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        auth()->user()->update($validatedData);
+
+        return response()->updated($validatedData);
+    }
+}
