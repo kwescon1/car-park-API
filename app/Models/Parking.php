dac93524a65bda12f8ae7dabbe7dfaea353c092e
Odame <kwesi.danquah@daaseng.com>
2023-06-27 15:58:02 +0100

    parking model

--- app/Models/Parking.php
+++ app/Models/Parking.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\UserParkingScope;
+use App\Observers\ParkingObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +14,31 @@ class Parking extends Model
     protected $guarded = ['id'];
 
     protected $casts = ['start_time' => 'datetime', 'stop_time' => 'datetime'];
+
+    protected static function booted()
+    {
+        self::observe(ParkingObserver::class);
+
+        static::addGlobalScope(new UserParkingScope);
+    }
+
+    public function zone()
+    {
+        return $this->belongsTo(Zone::class);
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->whereNull('stop_time')->latest();
+    }
+
+    public function scopeStopped($query)
+    {
+        return $query->whereNotNull('stop_time')->latest();
+    }
 }
